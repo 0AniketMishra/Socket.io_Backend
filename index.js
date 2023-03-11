914ccc1593bc1a9e21fae5ae141c343d18611643
ConnectionsLLC <111168015+ConnectionsLLC@users.noreply.github.com>
2023-03-11 07:27:45 +0000

    First Commit

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-const express = require('express'); 
-const port = 3000; 
-
-const app = express(); 
+const express = require('express');
+const port = 3000;
+const app = express();
 const bodyParser = require('body-parser');
 
-require('./db')
-require('./Models/User')
-require('./Models/Post')
-require('./Models/Message')
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
 
 
-const authRoutes = require('./routes/authRoutes')
-const uploadMediaRoutes = require('./routes/uploadMediaRoutes')
-const messageRoutes = require('./routes/messageRoutes');
+const io = new Server(httpServer, {});
 
 
-
-app.use(bodyParser.json())
-app.use(authRoutes)
-app.use(uploadMediaRoutes)
-app.use(messageRoutes)
+//
 
 
-app.get('/',(req,res) => { 
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
 
+io.on("connection", (socket) => {
+
+    console.log("USER CONNECTED - ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED - ", socket.id);
+    });
+
+    socket.on("join_room", (data) => {
+        console.log("USER WITH ID - ",socket.id,"JOIN ROOM - ", data.roomid);
+        socket.join(data);
+    });
+
+    socket.on("send_message", (data) => {
+        console.log("MESSAGE RECEIVED - ", data);
+        io.emit("receive_message", data);
+    });
+});
+
+
+httpServer.listen(3001);
+
+// ...
 app.listen(port, () => {
-    console.log("Server Runnning On Port "+port)
+    console.log("Server is running on port " + port);
 })
+
+
+
+// 6353f0c6a52cde6dafae64d2634c1d60f09a4f3ff40be517
+// 6353f0c6a52cde6dafae64d2634c1d60f09a4f3ff40be517
